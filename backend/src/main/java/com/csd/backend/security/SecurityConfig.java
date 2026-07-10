69f7f54b25bd200548c4ed4d944b59adb04f9451
Yash Shelar <yashshelar006@gmail.com>
2026-07-10 19:15:27 +0530

    Updated SecurityConfig.java

--- backend/src/main/java/com/csd/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/csd/backend/security/SecurityConfig.java
@@ -100,7 +100,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://csd-smart-slot-booking-frontend.vercel.app"
+                "https://csd-smart-slot-booking.vercel.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
